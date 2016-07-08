467808aa2296dcb893496c60a98f0b1e31e4ab8f
Aidan San <asan2@linux-a2.ews.illinois.edu>
2016-07-07 22:55:15 -0500

    added items to vector

--- leagueRoles.py
+++ leagueRoles.py
@@ -1,25 +1,36 @@
 import json, requests, dotenv, os
-
 dotenv.load_dotenv('.env')
 
-jMatchStng = open('seed-files/matches1.json').read()
-jMatch = json.loads(jMatchStng)
+def itemListToVector(l, d):
+    v = [None]*len(d.key())
+    for i in l:
+        v[d[i]] = 1
+    return v        
 
-matches = jMatch['matches']
+def main():
+    jMatchStng = open('seed-files/matches1.json').read()
+    jMatch = json.loads(jMatchStng)
 
-for m in matches:
-	for p in m['participants']:
-		lane = p['timeline']['lane']
-		role = p['timeline']['role']
-		items = [ p['stats']['item'+str(i)] for i in range(1, 7) ]
+    matches = jMatch['matches']
 
-#Items Corresponding to Summoner's Rift
-rItems = requests.get('https://global.api.pvp.net/api/lol/static-data/na/v1.2/item', params = {'itemListData': 'maps', 'api_key': os.environ['API_KEY']})
-jItems = json.loads(rItems.text)
+    for m in matches:
+        for p in m['participants']:
+            lane = p['timeline']['lane']
+            role = p['timeline']['role']
+            items = [ p['stats']['item'+str(i)] for i in range(1, 7) ]
 
-itemsSR = []
-for k, v in jItems['data'].items():
-	itemsSR += [k] if v['maps']['11'] else [] #Map 11 is New Summoner's Rift
+    #Items Corresponding to Summoner's Rift
+    rItems = requests.get('https://global.api.pvp.net/api/lol/static-data/na/v1.2/item', params = {'itemListData': 'maps', 'api_key': os.environ['API_KEY']})
+    jItems = json.loads(rItems.text)
 
-print(len(itemsSR))
+    itemsSR = []
+    for k, v in jItems['data'].items():
+        itemsSR += [k] if v['maps']['11'] else [] #Map 11 is New Summoner's Rift
 
+    itemidToIndex = {itemsSR[i]: i for i in range(len(itemsSR))}
+
+
+    print(len(itemsSR))
+
+if __name__ == '__main__':
+    main()
